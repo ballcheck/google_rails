35e943b037fd48417a146c619ca930c8986a8ef2
Ballcheck <ubuntu@ip-172-31-24-57.eu-west-1.compute.internal>
2015-03-20 11:22:51 +0000

    new default rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GoogleRails::Application.config.secret_token = '20184377e499125568be6e93008cfdfdf91590ce7ce3571bf287c328a8a154b6579498a459d8b9898f69ea4b32f9dd4bf86d7b4a398b1d11eb329706e145f716'
